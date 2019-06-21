68a9fa74fca45e79c8e393c7396405a8aa0bd51b
CRISTIANO APARECIDO <cristianoliveira063@gmail.com>
2019-06-21 00:09:41 -0400

    Configuração do template AdminLTE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Get Started'
+    },
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+    ]
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,29 @@ import { NgModule } from '@angular/core';
 
 import {AccordionModule} from 'primeng/accordion';
 
+import { adminLteConf } from './admin-lte.conf';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component';
+import {CoreModule} from './core/core.module';
+import {LayoutModule} from 'angular-admin-lte';
+import {LoadingPageModule, MaterialBarModule} from 'angular-loading-page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    AccordionModule
+    AccordionModule,
+    CoreModule,
+    LayoutModule.forRoot(adminLteConf),
+    LoadingPageModule, MaterialBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
